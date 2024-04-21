727bf4d2de5e78449b8563357adb0bb5a9256335
Zhanserik <220401@astanait.edu.kz>
2024-04-22 02:18:35 +0500

    Added a handler for the "about us", "help", "rules" buttons.

--- handlers/message_handlers.py
+++ handlers/message_handlers.py
@@ -6,4 +6,18 @@ from aiogram.fsm.context import FSMContext
 
 
 async def register_message_handlers(dp, bot, mongo):
-    pass
+    @dp.message(F.text.lower() == "rules")
+    async def rules(message: types.Message):
+        with open("./files/txt/rules_message.txt", "r", encoding="utf-8") as file:
+            rules_text = file.read()
+        await message.reply(rules_text)
+
+    @dp.message(F.text.lower() == "help")
+    async def help(message: types.Message):
+        await message.reply("For any questions please contact me: \n\nTG: https://t.me/xwikkwix")
+
+    @dp.message(F.text.lower() == "about us")
+    async def about_us(message: types.Message):
+        with open(".//files//txt//about_us_message.txt", "r", encoding="utf-8") as file:
+            about_us_text = file.read()
+        await message.reply(about_us_text)
