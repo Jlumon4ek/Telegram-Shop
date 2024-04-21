a7e19dd3519612c63b7dcc2ad9d569a7c28b2f4e
Zhanserik <220401@astanait.edu.kz>
2024-04-22 02:15:44 +0500

    Added command handler /help, /cancel

--- handlers/command_handlers.py
+++ handlers/command_handlers.py
@@ -19,3 +19,15 @@ async def register_command_handlers(dp, bot, mongo):
         keyboard = await main_buttons(mongo, user_id)
 
         await message.reply(message_text, reply_markup=keyboard)
+
+    @dp.message(Command("cancel"))
+    async def cancel(message: types.Message,  state: FSMContext):
+        await message.reply("Operation canceled")
+        user_id = message.from_user.id
+        keyboard = await main_buttons(user_id)
+        await state.clear()
+        await message.reply("Hello! How can I help?", reply_markup=keyboard)
+
+    @dp.message(Command("help"))
+    async def help_command(message: types.Message):
+        await message.reply("For any questions please contact me: \n\nTG: https://t.me/xwikkwix")
